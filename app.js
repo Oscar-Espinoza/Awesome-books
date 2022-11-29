bfbe2619064f8777e2a5e73df0bfaa5019e93c63
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-29 18:34:29 -0300

    linter errors solved

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+/* eslint-disable max-classes-per-file */
 class Book {
   constructor(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
-  }
-}
+  };
+};
 
 const initialBooks = [
-  new Book (0, 'book1', 'author1'),
-  new Book (1, 'book2', 'author2'),
+  new Book(0, 'book1', 'author1'),
+  new Book(1, 'book2', 'author2'),
 ];
 
-let booksArr = JSON.parse(localStorage.getItem('books'))
+let booksArr = JSON.parse(localStorage.getItem('books'));
 
 const booksList = document.getElementById('books-list');
 
-class Methods {
-  constructor() {}
+const removeBook = (book) => {
+  const bookIndex = Array.from(booksList.children).indexOf(book);
+  bookMethods.remove.call(booksArr[bookIndex]);
+  booksList.removeChild(book);
+};
 
+class Methods {
   remove() {
     booksArr = booksArr.filter((book) => book !== this);
     localStorage.setItem('books', JSON.stringify(booksArr));
@@ -35,10 +40,10 @@ class Methods {
   addBookToDom() {
     const newBook = document.createElement('li');
     newBook.classList.add('book');
-    if (this.id%2 === 0) {
-      newBook.classList.add('odd-bg')
+    if (this.id % 2 === 0) {
+      newBook.classList.add('odd-bg');
     } else {
-      newBook.classList.add('pair-bg')
+      newBook.classList.add('pair-bg');
     }
     newBook.setAttribute('id', this.id);
     newBook.innerHTML = `
@@ -47,27 +52,21 @@ class Methods {
     `;
     newBook.addEventListener('click', () => removeBook(newBook));
     booksList.appendChild(newBook);
+  }
+}
+
+const bookMethods = new Methods();
+
+const createList = (booksArr) => {
+  if (booksArr === null || booksArr.length === 0) {
+    booksArr = initialBooks;
+    localStorage.setItem('books', JSON.stringify(booksArr));
   };
-}
 
-const bookMethods = new Methods()
-
-if (booksArr === null || booksArr.length === 0) {
-  booksArr = initialBooks
-  localStorage.setItem('books', JSON.stringify(booksArr));
-}
-
-const createList = () => {
   booksArr.forEach((book) => {
     bookMethods.addBookToDom.call(book);
   });
 };
-
-const removeBook = (book) => {
-  const bookIndex = Array.from(booksList.children).indexOf(book);
-  bookMethods.remove.call(booksArr[bookIndex]);
-  booksList.removeChild(book);
-}
 
 document.querySelectorAll('remove-btn').forEach((removeBtn) => {
   removeBtn.addEventListener('click', removeBook(this.parentNode));
@@ -77,7 +76,7 @@ document.getElementById('add-book-btn').addEventListener('click', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const id = booksArr.length;
-  const newBook = new Book(id, title, author)
+  const newBook = new Book(id, title, author);
   bookMethods.addBookToDom.call(newBook);
   bookMethods.addBookStorage.call(newBook);
 });
