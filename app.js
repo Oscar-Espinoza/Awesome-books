14054d91cd93b9dea05a2535640ed9babf3399f8
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-11-28 23:52:21 -0300

    Linters added

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import initialBooks from './books.json' assert {type: 'json'};
 const booksListEl = document.getElementById('books-list');
 if (localStorage.getItem('books') === null) {
-  localStorage.setItem('books', JSON.stringify(initialBooks))
+  localStorage.setItem('books', JSON.stringify(initialBooks));
 }
 
 let booksArr = JSON.parse(localStorage.getItem('books'));
 
 
 const remove = (removedBook) => {
-  booksArr = booksArr.filter( (book) => book.id != removedBook.id)
-  localStorage.setItem('books', JSON.stringify(booksArr))
+  booksArr = booksArr.filter( (book) => book.id != removedBook.id);
+  localStorage.setItem('books', JSON.stringify(booksArr));
   booksListEl.removeChild(removedBook);
 };
 
@@ -20,40 +20,40 @@ const addBookStorage = (title, author, index) => {
       author: author,
       id: index,
     }
-  )
-  localStorage.setItem('books', JSON.stringify(booksArr))
-}
+  );
+  localStorage.setItem('books', JSON.stringify(booksArr));
+};
 
 const addBookToDom = (title, author, index) => {
   const newBookEl = document.createElement('li');
   newBookEl.classList.add('book');
-  newBookEl.setAttribute('id', index)
+  newBookEl.setAttribute('id', index);
   newBookEl.innerHTML = `
   <h2 class="book-title">${title}</h2>
   <p class="book-author">${author}</p>
   <button class="remove-btn">remove</button>
   `;
-  newBookEl.addEventListener('click', () => remove(newBookEl))
+  newBookEl.addEventListener('click', () => remove(newBookEl));
   booksListEl.appendChild(newBookEl);  
-}
+};
 
 const createList = () => {
   booksArr.forEach((book) => {
     addBookToDom(book.title, book.author, book.id);
   });
-}
+};
 
 document.querySelectorAll('remove-btn').forEach(removeBtn => {
-  removeBtn.addEventListener('click', remove(removeBtn))
+  removeBtn.addEventListener('click', remove(removeBtn));
 });
 
 document.getElementById('add-book').addEventListener('click', () => {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const id = booksArr.length
-  console.log(id)
-  addBookToDom(title, author, id)
-  addBookStorage(title, author, id)
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const id = booksArr.length;
+  console.log(id);
+  addBookToDom(title, author, id);
+  addBookStorage(title, author, id);
 })
 
 createList();
